02ddfb27db6d42837d50e2b4a143fe5b604e87e0
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-03-12 21:40:21 +0700

    ex6 Huong dan

--- Ex_6_ListViewDemo/app/src/main/java/till/edu/ex_6_listviewdemo/MainActivity.java
+++ Ex_6_ListViewDemo/app/src/main/java/till/edu/ex_6_listviewdemo/MainActivity.java
@@ -1,6 +1,13 @@
 package till.edu.ex_6_listviewdemo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +16,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> dsTenTinhThanhVN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+        //Hien thi dl len list  view
+        //B1: can co du lieu
+        //ArrayList<String> dsTenTinhThanhVN; //khai bao
+        dsTenTinhThanhVN = new ArrayList<>(); //tao the hien cu the, xin moi
+        //Them du lieu
+        dsTenTinhThanhVN.add("Hà Nội");
+        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+        dsTenTinhThanhVN.add("Đồng Nai");
+        dsTenTinhThanhVN.add("Bình Thuận");
+        dsTenTinhThanhVN.add("Ninh Thuận");
+        dsTenTinhThanhVN.add("Nha Trang");
+        dsTenTinhThanhVN.add("Tiền Giang");
+
+        //B2: Tao adapter
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                dsTenTinhThanhVN);
+
+        //B3 Gan vaoo dk hien thi ListView
+        //3.1 Tim
+        ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
+        //Gan
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        //3.3 Lang nghe va xu li su kien tuong tac
+        //Gan bo lang nghe vao
+        
+    };
+
 }
