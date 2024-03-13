0244076c44df09953e50654dcc1b5f59bd9f1049
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-03-13 19:55:40 +0700

    Hoan thien ex6+

--- Ex_6Cong_ListViewThemXoaSua/app/src/main/java/till/edu/ex_6cong_listviewthemxoasua/MainActivity.java
+++ Ex_6Cong_ListViewThemXoaSua/app/src/main/java/till/edu/ex_6cong_listviewthemxoasua/MainActivity.java
@@ -28,12 +28,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         setContentView(R.layout.activity_main);
         btnThem = (Button)findViewById(R.id.btnThem);
         btnSua = (Button)findViewById(R.id.btnSua);
