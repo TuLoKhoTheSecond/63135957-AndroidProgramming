4a315ff4d2789f32edf8f869a5bca86e77fb73cd
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-03-23 18:56:31 +0700

    Bắt đầu làm câu 1 đề thi giữa kì
    App máy tính bỏ túi (java+swing)

---
+++ Cau1_AppMayTinhBoTui/src/ntu/NgoNguyenAnhTu63135957/AppMayTinhBoTui.java
@@ -0,0 +1,43 @@
+package ntu.NgoNguyenAnhTu63135957;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AppMayTinhBoTui extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppMayTinhBoTui frame = new AppMayTinhBoTui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AppMayTinhBoTui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
