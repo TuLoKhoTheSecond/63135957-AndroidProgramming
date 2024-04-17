28a284fb40928ea5b160d5ed21d79ed5a89fc1f5
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-04-17 10:39:33 +0700

    update fragment

--- SlideFragments/app/src/main/java/ntu/ngonguyenanhtu63135957/slidefragments/MainActivity.java
+++ SlideFragments/app/src/main/java/ntu/ngonguyenanhtu63135957/slidefragments/MainActivity.java
@@ -2,23 +2,50 @@ package ntu.ngonguyenanhtu63135957.slidefragments;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+public class MainActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 5;
 
-public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager);
+
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
     }
 }
---
+++ SlideFragments/app/src/main/java/ntu/ngonguyenanhtu63135957/slidefragments/ScreenSlidePageFragment.java
@@ -0,0 +1,18 @@
+package ntu.ngonguyenanhtu63135957.slidefragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.slide_page_screen, container, false);
+    }
+}
