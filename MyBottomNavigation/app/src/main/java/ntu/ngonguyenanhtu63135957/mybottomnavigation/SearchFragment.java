c498b4897981615b5ce7ba2731a9d9d2ca84c3ce
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-04-26 08:22:27 +0700

    hoan thien

---
+++ MyBottomNavigation/app/src/main/java/ntu/ngonguyenanhtu63135957/mybottomnavigation/SearchFragment.java
@@ -0,0 +1,19 @@
+package ntu.ngonguyenanhtu63135957.mybottomnavigation;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.searchfragment, container, false);
+    }
+}
